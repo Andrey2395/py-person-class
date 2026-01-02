be3c1cd73e1c0cb247961cc52c051d2dcb341945
Andr <TorqueX369@outlook.com>
2026-01-02 19:33:37 +0200

    fix Solution

--- app/main.py
+++ app/main.py
@@ -8,21 +8,21 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    person_list = []
-    for person_data in people:
-        name = person_data["name"]
-        age = person_data["age"]
-        person = Person(name, age)
-        person_list.append(person)
+    person_list = [
+        Person(person_data["name"], person_data["age"])
+        for person_data in people
+    ]
 
     for person_data in people:
         name = person_data["name"]
         person = Person.people[name]
 
-        if "wife" in person_data and person_data["wife"] is not None:
-            person.wife = Person.people[person_data["wife"]]
-        if "husband" in person_data and person_data["husband"] is not None:
-            person.husband = Person.people[person_data["husband"]]
+        wife_name = person_data.get("wife")
+        if wife_name is not None:
+            person.wife = Person.people[wife_name]
+        husband_name = person_data.get("husband")
+        if husband_name is not None:
+            person.husband = Person.people[husband_name]
     return person_list
 
 
